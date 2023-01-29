0a354a048395f20b376329daeaee8f0bca327caf
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-01-29 11:33:31 -0600

    AppJs created and components imported

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import './App.css' will uncomment when App css is created
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Project from './components/Project';
+
+function App(){
+    return (
+        <>
+        <Header/>
+        <About/>
+        <Project/> 
+        <Contact/>
+        <Footer/>
+        </>
+    )
+}
+export default App;
