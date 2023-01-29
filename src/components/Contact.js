5113f9009b8e69773acd39e4cb61ab35857446ba
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-01-29 12:04:40 -0600

    contact componet created_ still needs tailwind

---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section>
+            <div >
+                <h2 >
+                    Contact Me:
+                </h2>
+                <p>
+                    I am currently looking for a Front End Developer Postion. My contact info 
+                    is below if you would like to get in touch. Thank you for taking time to 
+                    look at this. Hope you enjoyed the Star Wars puns through out. 
+                </p>
+
+                <p >
+                    <span>Email:</span> Matthew.d.waldron@gmail.com
+                </p>
+                <p >
+                    <span>Phone:</span> 414-897-6640
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
