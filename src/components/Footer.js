f6545fd651c581dce52b93d57fe207fcecc52b10
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2023-01-29 12:07:49 -0600

    created Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer =()=> {
+    return (
+        <section>
+            <div>
+                &copy 2023 || Thank you, Wally. 
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
